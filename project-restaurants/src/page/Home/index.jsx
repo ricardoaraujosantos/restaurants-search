e4dd425f1152da93b6ad309d4e92d307733ed6e6
Ricardo <ricardoaraujo3713@gmail.com>
2021-09-02 23:42:22 -0300

    Criando aside inserindo logo instalando @material/react-text-field para utilizar um imput de busca estilizado

--- project-restaurants/src/page/Home/index.jsx
+++ project-restaurants/src/page/Home/index.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Container } from './style';
+import { Container, Search } from './style';
+
+import TextField, {Input} from '@material/react-text-field';
+
+import Logo from '../../assets/logo.svg'
 
 
-const Home = () => <Container>Home page</Container>;
+const Home = () => {
+
+    const [inputValue, setInputValue] = useState('cachorro');
+
+    return(
+            <Container>
+                <Search>
+                    <img src={Logo} alt="Logo do restaurante"/>
+                        <TextField
+                            label='Pesquisar'
+                            // onTrailingIconSelect={() => this.setState({value: ''})}
+                            // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+                        >
+                        <Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)} />
+                        </TextField>
+                </Search>
+            </Container>
+
+    );
+};
 
 export default Home;
--- project-restaurants/src/page/Home/style.js
+++ project-restaurants/src/page/Home/style.js
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 
-export const Container = styled.div`
+export const Container = styled.aside`
 
-    background-color: ${(props) => props.theme.colors.primary};
-
-    color: ${(props) => props.theme.colors.text};
-    
-    font-Family: ${(props) => props.theme.fonts.fontFamily};
-    
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: scroll;
 `;
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #fff;
+    padding: 14px;
+`;
+
+
